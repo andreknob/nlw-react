c0a96bdc06d10f2d4e7f663f73ff97f1505a666d
André Knob <knobandre@gmail.com>
2025-02-22 02:14:30 -0300

    chore: breaking down components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,7 @@
+import { Radio } from 'lucide-react';
 import Image from 'next/image';
-
-import { Button } from '@/components/button';
-import { InputField, InputIcon, InputRoot } from '@/components/input';
-import { ArrowRight, Mail, Radio, User } from 'lucide-react';
-import logo from '../assets/logo.svg';
+import logo from '../../assets/logo.svg';
+import { SubscriptionForm } from './subscription-form';
 
 export default function Home() {
   return (
@@ -35,32 +33,8 @@ export default function Home() {
             emerging technologies, with lectures, workshops and hackathons.
           </p>
         </div>
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Subscription
-          </h2>
 
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User />
-              </InputIcon>
-              <InputField type="text" placeholder="Full Name" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <Mail />
-              </InputIcon>
-              <InputField type="email" placeholder="Email" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirm
-            <ArrowRight />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   );
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,11 +1,8 @@
-import { IconButton } from '@/components/icon-button';
-import { InputField, InputIcon, InputRoot } from '@/components/input';
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react';
 import Image from 'next/image';
 import logo from '../../assets/logo.svg';
-import bronze from '../../assets/medal-bronze.svg';
-import gold from '../../assets/medal-gold.svg';
-import silver from '../../assets/medal-silver.svg';
+import { InviteLinkInput } from './invite-link-input';
+import { Ranking } from './ranking';
+import { Stats } from './stats';
 
 export default function InvitePage() {
   return (
@@ -33,101 +30,13 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InputRoot>
-            <InputIcon>
-              <Link className="size-5" />
-            </InputIcon>
+          <InviteLinkInput />
 
-            <InputField
-              readOnly
-              defaultValue="http://localhost:3000/invite/5196084198"
-            />
-
-            <IconButton className="-mr-2">
-              <Copy className="size-5" />
-            </IconButton>
-          </InputRoot>
-
-          <div className="grid gap-3 md:grid-cols-3">
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Link Accesses
-              </span>
-
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Subscriptions made
-              </span>
-
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3º
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Ranking position
-              </span>
-
-              <Medal className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-          </div>
+          <Stats />
         </div>
       </div>
 
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Subscriptions Ranking
-        </h2>
-
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Name 1
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1000
-            </span>
-
-            <Image src={gold} alt="" className="absolute top-0 right-8" />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2º</span> | Name 2
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              900
-            </span>
-
-            <Image src={silver} alt="" className="absolute top-0 right-8" />
-          </div>
-
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3º</span> | Name 3
-            </span>
-
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              700
-            </span>
-
-            <Image src={bronze} alt="" className="absolute top-0 right-8" />
-          </div>
-        </div>
-      </div>
+      <Ranking />
     </div>
   );
 }
